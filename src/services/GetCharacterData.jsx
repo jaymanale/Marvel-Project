612ab4175b008d0052b5935a5db05bc33ecb7371
jaymanale <jaymanale29@gmail.com>
2020-07-10 15:15:00 +0530

    Request change  to HTTPS

--- src/services/GetCharacterData.jsx
+++ src/services/GetCharacterData.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const GetCharacterData = () => {
   let URL =
-    'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=957f3e2ab0da2e77b9ef76d8c3550706&hash=492211148eb06fc7144c685e346da6b4';
+    'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=957f3e2ab0da2e77b9ef76d8c3550706&hash=492211148eb06fc7144c685e346da6b4';
 
   return axios.get(URL);
 };
