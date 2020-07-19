ec282b0b6dac80c5505cbcb90725f00b5d2226b4
jaymanale <jaymanale29@gmail.com>
2020-07-19 20:19:53 +0530

    🔥 Loading component improved

--- src/common/Loading.jsx
+++ src/common/Loading.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import loadingGif from './../resources/loading.gif';
+import loadingGif from './../resources/Spinner.gif';
 
 const Loading = () => {
   return (
     <div className="container mt-2">
       <div className="row">
-        <div className="col-sm-4 col-md-4 col-lg-4 offset-4">
-          <img
-            src={loadingGif}
-            className="img-fluid"
-            width="300"
-            height="300"
-            alt="loading..."
-          />
-          <p className="font-weight-bold text-center mt-1">Loading...</p>
+        <div className="col-sm-4 col-md-4 col-lg-4 offset-sm-4 offset-md-4 offset-lg-4 text-center">
+          <img src={loadingGif} className="img-fluid" alt="loading..." />
+          <p className="font-weight-bold text-white mt-1">Loading...</p>
         </div>
       </div>
     </div>
