96a4ee6654773076d84974fd5a2e984e89359026
jaymanale <jaymanale29@gmail.com>
2020-07-19 19:24:04 +0530

    👍 Footer Added

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Footer = () => {
-  return <h4>This is Footer</h4>;
+  return (
+    <div className="text-white p-2">
+      <p>
+        Made with
+        <span role="img" aria-label="smile" className="mx-1">
+          😄
+        </span>
+        by MARVEL fan
+        <span className="mx-1 text-info">
+          <a href="https://twitter.com/JayManale"> @JayManale</a>
+        </span>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- src/common/LoadMore.jsx
+++ src/common/LoadMore.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadMore = (props) => (
   <div className="text-center mt-1">
-    <button className="btn btn-secondary" onClick={props.onHandleLoadMore}>
+    <button className="btn btn-info" onClick={props.onHandleLoadMore}>
       Load More...
     </button>
   </div>
