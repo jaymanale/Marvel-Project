b9e413610618415c03d3bfd6cf0b90bc4ec56fbe
jaymanale <jaymanale29@gmail.com>
2020-07-20 08:33:55 +0530

    Card UI Improvement, Extra info added in Character

--- src/common/MarvelCard.jsx
+++ src/common/MarvelCard.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const MarvelCard = (props) => {
   const { title, thumbnail } = props.cardData;
   return (
-    <div className="card text-center my-1 cardShadow">
-      <div className="card-body">
-        <div className="m-2">
+    <div className="card text-center my-2 cardShadow">
+      <div className="card-body p-0">
+        <div>
           {thumbnail && (
             <img
               src={thumbnail.path + '.' + thumbnail.extension}
@@ -14,7 +14,9 @@ const MarvelCard = (props) => {
             />
           )}
         </div>
-        <h5 className="card-title font-weight-lighter text-dark">{title}</h5>
+        <h5 className="card-title font-weight-lighter text-dark mt-2">
+          {title}
+        </h5>
       </div>
     </div>
   );
--- src/components/CharacterInfo.jsx
+++ src/components/CharacterInfo.jsx
@@ -4,12 +4,20 @@ import Base from './../common/Base';
 
 const CharacterInfo = (props) => {
   console.log('props:', props);
-  const { name, description, thumbnail, comics } = props.location.state;
+  const {
+    name,
+    description,
+    thumbnail,
+    comics,
+    events,
+    series,
+    stories,
+  } = props.location.state;
   return (
     <Base>
       <div className="container">
         <div className="row">
-          <div className="col-sm-12 col-md-5 col-lg-5 offset-md-1 offset-lg-1">
+          <div className="col-sm-12 col-md-5 col-lg-5 offset-md-1 offset-lg-1 cardShadow p-0">
             {thumbnail && (
               <img
                 src={thumbnail.path + '.' + thumbnail.extension}
@@ -18,19 +26,44 @@ const CharacterInfo = (props) => {
               />
             )}
           </div>
-          <div className="col-sm-12 col-md-5 col-lg-5 offset-md-1 offset-lg-1 text-white">
-            <h2 className="display-2">{name}</h2>
+          <div className="col-sm-12 col-md-5 col-lg-5 offset-md-1 offset-lg-1 text-dark mt-4">
+            <h2 className="display-4">{name}</h2>
             <p>
               {description ? description : <i>Description not available</i>}
             </p>
-            <div className="row">
-              <div>
-                <p>Comics</p>
-                <p>{comics.items.length ? comics.items.length : 0}</p>
-              </div>
-
-              <p>Comics</p>
-              <p>{comics.items.length ? comics.items.length : 0}</p>
+            <div className="row text-center">
+              {comics.items.length ? (
+                <div className="col-3">
+                  <p className="text-strong mb-0 textColorGradiant">Comics</p>
+                  <p>{comics.available}</p>
+                </div>
+              ) : (
+                ''
+              )}
+              {events.items.length ? (
+                <div className="col-3">
+                  <p className="text-strong mb-0 textColorGradiant">Events</p>
+                  <p>{events.available}</p>
+                </div>
+              ) : (
+                ''
+              )}
+              {series.items.length ? (
+                <div className="col-3">
+                  <p className="text-strong mb-0 textColorGradiant">Series</p>
+                  <p>{series.available}</p>
+                </div>
+              ) : (
+                ''
+              )}
+              {stories.items.length ? (
+                <div className="col-3">
+                  <p className="text-strong mb-0 textColorGradiant">Stories</p>
+                  <p>{stories.available}</p>
+                </div>
+              ) : (
+                ''
+              )}
             </div>
           </div>
         </div>
