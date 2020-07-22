ae02f7a2654e7345f45d0cccb5c7432813ac906d
jaymanale <jaymanale29@gmail.com>
2020-07-22 09:54:58 +0530

    Home page new character added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import Base from './../common/Base';
-import IronMan from './../resources/ironman.webp';
-import Captain from './../resources/captain.webp';
-import Doctor from './../resources/doctorstrange.webp';
-import Hulk from './../resources/hulk.webp';
-import Thor from './../resources/thor.webp';
-import BlackPanther from './../resources/blackpanther.webp';
-import WarMachine from './../resources/warmachine.webp';
-import NickFury from './../resources/nickfury.webp';
+import IronMan from './../resources/ironman.jpg';
+import Captain from './../resources/captain.jpg';
+import Hulk from './../resources/hulk.jpg';
+import Thor from './../resources/thor.jpg';
+import BlackPanther from './../resources/blackpanther.jpg';
+import Deadpool from './../resources/deadpool.jpg';
+import NickFury from './../resources/nickfury.jpg';
+import DoctorStrange from './../resources/doctorstrange.jpg';
 
 const Home = () => {
   const heroMap = [
     { id: 1, name: 'Iron Man', imgUrl: IronMan },
     { id: 2, name: 'Captain America', imgUrl: Captain },
-    { id: 3, name: 'Doctor Strange', imgUrl: Doctor },
+    { id: 3, name: 'Deadpool', imgUrl: Deadpool },
     { id: 4, name: 'Hulk', imgUrl: Hulk },
     { id: 5, name: 'Thor', imgUrl: Thor },
     { id: 6, name: 'Black Panther', imgUrl: BlackPanther },
-    { id: 7, name: 'War Machine', imgUrl: WarMachine },
+    { id: 7, name: 'Doctor Strange', imgUrl: DoctorStrange },
     { id: 8, name: 'Nick Fury', imgUrl: NickFury },
   ];
 
@@ -28,7 +28,7 @@ const Home = () => {
           {heroMap.map((hero) => (
             <div
               key={hero.id}
-              className="col-sm-3 col-md-3 col-lg-3 text-center mt-4"
+              className="col-sm-3 col-md-3 col-lg-3 text-center mt-4 cardShadow"
             >
               <img className="img-fluid" src={hero.imgUrl} alt={hero.imgUrl} />
               <p>{hero.name}</p>
