b5e8e2d33dc3ea2f852ae88f7cdf3ae509110a34
jaymanale <jaymanale29@gmail.com>
2020-07-26 13:16:42 +0530

    Testing API Hiding

--- src/services/GetData.jsx
+++ src/services/GetData.jsx
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const GetData = async ({ type, pageOffset, orderBy }) => {
-  let URL = `https://gateway.marvel.com/v1/public/${type}?orderBy=${orderBy}&ts=1&offset=${pageOffset}&limit=10&apikey=${process.env.API_KEY}&hash=${process.env.HASH_KEY}`;
+  let URL = `https://gateway.marvel.com/v1/public/${type}?orderBy=${orderBy}&ts=1&offset=${pageOffset}&limit=10&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH_KEY}`;
 
   const response = await axios.get(URL);
   return response.data.data.results;
 };
 
 export const getCharactorById = async (charactorId) => {
-  let URL = `https://gateway.marvel.com/v1/public/characters/${charactorId}?&ts=1&apikey=${process.env.API_KEY}&hash=${process.env.HASH_KEY}`;
+  let URL = `https://gateway.marvel.com/v1/public/characters/${charactorId}?&ts=1&apikey=${process.env.REACT_APP_API_KEY}&hash=${process.env.REACT_APP_HASH_KEY}`;
 
   const response = await axios.get(URL);
   return response.data.data.results[0];
